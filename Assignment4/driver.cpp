b1086855d4bd13573a5c5ccc5b91f0c8e07cb065
Adam Krivoshein <zike10@gmail.com>
2021-01-31 19:34:40 -0800

    Added testing in main function.

--- Assignment4/driver.cpp
+++ Assignment4/driver.cpp
@@ -7,11 +7,25 @@ using std::endl;
 
 int main() {
     linkedList<int> myList;
+    myList.add(3, 0);
     myList.add(1, 0);
-    myList.add(2, 0);
-    myList.add(3, 2);
-    myList.add(4, 1);
+    myList.add(4, 2);
+    myList.add(2, 1);
+    cout << "Get function test:" << endl;
+    cout << myList.get(0) << endl;
     cout << myList.get(1) << endl;
+    cout << myList.get(2) << endl;
+    cout << myList.get(3) << endl;
+    cout << myList.get(4) << endl;
+    cout << "Remove and get function test:" << endl;
+    cout << myList.get(0) << endl;
+    cout << myList.remove(0) << endl;
+    cout << myList.get(1) << endl;
+    cout << myList.remove(1) << endl;
+    cout << myList.get(20) << endl;
+    cout << myList.remove(20) << endl;
+    cout << myList.get(0) << endl;
+    cout << myList.remove(0) << endl;
 
     return 0;
 }
