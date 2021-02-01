071117d6ab45f58b2c529c32a975c9c11689e969
Adam Krivoshein <adamskrivo@gmail.com>
2021-01-31 20:38:41 -0800

    Added big O analysis.

--- Assignment4/linkedList.h
+++ Assignment4/linkedList.h
@@ -15,26 +15,32 @@ class linkedList {
             front = NULL;
         }
 
+        //O(n)
         void add(T val, int index) {
+            //If statement so doesn't contribute to big O complexity.
             if (front == NULL && index == 0) {
                 front = new node<T>;
                 front->val = val;
                 front->next = NULL;
             }
+            //If statement so doesn't contribute to big O complexity.
             else if (index == 0 && front != NULL) {
                 node<T>* oldFront = front;
                 front = new node<T>;
                 front->val = val;
                 front->next = oldFront;
             }
+            //If statement so doesn't contribute to big O complexity.
             else if (front != NULL) {
                 node<T>* currentNode = front;
+                //For loop is O(n)
                 for (int i = 1; i < index; i++) {
                     if (currentNode->next != NULL)
                         currentNode = currentNode->next;
                     else
                         return;
                 }
+                //If statement so doesn't contribute to big O complexity.
                 if (currentNode->next != NULL) {
                     node<T>* newNode = new node<T>;
                     newNode->val = val;
@@ -50,12 +56,16 @@ class linkedList {
             }
         }
 
+        //O(n)
         T remove(int index) {
             T val = 0;
+            //If statement so doesn't contribute to big O complexity.
             if (front != NULL) {
                 node<T>* currentNode = front;
                 //If index is 0
+                //If statement so doesn't contribute to big O complexity.
                 if (index == 0) {
+                    //If statement so doesn't contribute to big O complexity.
                     if (currentNode->next != NULL)
                         front = currentNode->next;
                     val = currentNode->val;
@@ -63,12 +73,14 @@ class linkedList {
                     return val;
                 }
                 //Iterating through the list, finding the node before the one to be removed.
+                //For loop is O(n)
                 for (int i = 1; i < index; i++) {
                     if (currentNode->next != NULL)
                         currentNode = currentNode->next;
                     else
                         return val;
                 }
+                //If statement so doesn't contribute to big O complexity.
                 //Removing the node...
                 if (currentNode->next != NULL) {
                     node<T>* removedNode = currentNode->next;
@@ -83,12 +95,15 @@ class linkedList {
             return val;
         }
 
+        //O(n)
         T get(int index) {
             node<T>* currentNode = front;
             T bad = 0;
-
+            //If statement so doesn't contribute to big O complexity.
             if (currentNode != NULL) {
+                //For loop is O(n)
                 for (int i = 0; i < index; i++) {
+                    //If statement so doesn't contribute to big O complexity.
                     if (currentNode->next != NULL)
                         currentNode = currentNode->next;
                     else
@@ -100,10 +115,13 @@ class linkedList {
         }
 
         //Strictly for testing purposes only
+        //O(n)
         void displayList() {
             node<T>* currentNode = front;
+            //If statement so doesn't contribute to big O complexity.
             if (currentNode != NULL) {
                 cout << currentNode->val << endl;
+                //While loop is O(n)
                 while (currentNode->next != NULL) {
                     currentNode = currentNode->next;
                     cout << currentNode->val << endl;
