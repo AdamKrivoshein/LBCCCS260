15f80bfbf0a1ba99257c7a3aea1a6a7c66b700d2
Adam Krivoshein <adamskrivo@gmail.com>
2021-02-22 12:44:56 -0800

    Working binary search tree.

--- Assignment6/binarysearchtree.cpp
+++ Assignment6/binarysearchtree.cpp
@@ -42,29 +42,139 @@ class BinarySearchTree {
         //Inputs: int (value to be removed)
         //Outputs: None.
         void remove(int val) {
-            //Node<int>* removeNode = findNode();
+            //Find parent of node to be removed
+            Node<int>* nodeParent = findNodeParent(root, val);
+            Node<int>* removeNode;
+            char side;
+            
+            //If removeNode is not root (In which case it can't have a parent)
+            if (nodeParent != nullptr) {
+                //Determine and store which child should be removed
+                if (nodeParent->left != nullptr) {
+                    if (nodeParent->left->val == val) {
+                        removeNode = nodeParent->left;
+                        side = 'l';
+                    }
+                }
+                if (nodeParent->right != nullptr) {
+                    if (nodeParent->right->val == val) {
+                        removeNode = nodeParent->right;
+                        side = 'r';
+                    }
+                }
+
+                //1st Removal Case: Node has no children
+                if (removeNode->left == nullptr && removeNode->right == nullptr) {
+                    if (side == 'l')
+                        nodeParent->left = nullptr;
+                    else
+                        nodeParent->right = nullptr;
+                    delete removeNode;
+                    return;
+                }
+            } 
+            //Edge case when removeNode is root, but has no children
+            else {
+                removeNode = root;
+
+                if (removeNode->left == nullptr && removeNode->right == nullptr) {
+                    delete removeNode;
+                    root = nullptr;
+                    return;
+                }
+            }
+            //2nd Removal Case: Node has children. Reorganizing is needed
+            Node<int>* replacementNode;
+            if (removeNode->left != nullptr) {
+                if (removeNode->left->right != nullptr)
+                    replacementNode = findLargest(removeNode->left);
+                else {
+                    replacementNode = removeNode->left;
+                    if (removeNode->left->left != nullptr)
+                        removeNode->left = removeNode->left->left;
+                    else
+                        removeNode->left = nullptr;
+                }
+            }
+            else if (removeNode->right != nullptr) {
+                if (removeNode->right->left != nullptr)
+                    replacementNode = findSmallest(removeNode->right);
+                else {
+                    replacementNode = removeNode->right;
+                    if (removeNode->right->right != nullptr)
+                        removeNode->right = removeNode->right->right;
+                    else
+                        removeNode->right = nullptr;
+                }
+            }
+            removeNode->val = replacementNode->val;
+            delete replacementNode;
+
+            return;
         }
 
         //Function: Finds and returns a node with a specified value.
         //Inputs: Node* (root or recursive node), int (value to be found)
         //Outputs: Node*
-        /*Node<int>* findNode(Node<int>* node, int val) {
-            
-        }*/
+        Node<int>* findNodeParent(Node<int>* node, int val) {
+            if (val < node->val) {
+                if (node->left != nullptr) {
+                    if (val != node->left->val)
+                        return findNodeParent(node->left, val);
+                    else
+                        return node;
+                }
+                return nullptr;
+            }
+            else if (val > node->val) {
+                if (node->right != nullptr) {
+                    if (val != node->right->val)
+                        return findNodeParent(node->right, val);
+                    else
+                        return node;
+                }
+                return nullptr;
+            }
+            return nullptr;
+        }
 
         //Function: Finds & returns the largest node below the given node.
-        //Inputs: Node* (The left node of a node to be removed)
+        //Inputs: Node* (The left node of a node to be removed, ONLY if the node passed has a child to the right)
         //Outputs: Node*.
-        /*Node<int>* findLargest(Node<int>* node) {
-
-        }*/
+        Node<int>* findLargest(Node<int>* node) {
+            if (node->right != nullptr) {
+                if (node->right->right != nullptr)
+                    return findLargest(node->right);
+                else {
+                    Node<int>* tempNode = node->right;
+                    if (node->right->left != nullptr)
+                        node->right = node->right->left;
+                    else
+                        node->right = nullptr;
+                    return tempNode;
+                }
+            }
+            return nullptr;     //In theory this should never happen
+        }
 
         //Function: Finds & returns the smallest node below the given node.
-        //Inputs: Node* (The right node of a node to be removed)
+        //Inputs: Node* (The right node of a node to be removed, ONLY if the node passed has a child to the left)
         //Outputs: Node*.
-        /*Node<int>* findSmallest(Node<int>* node) {
-
-        }*/
+        Node<int>* findSmallest(Node<int>* node) {
+            if (node->left != nullptr) {
+                if (node->left->left != nullptr)
+                    return findLargest(node->left);
+                else {
+                    Node<int>* tempNode = node->left;
+                    if (node->left->right != nullptr)
+                        node->left = node->left->right;
+                    else
+                        node->left = nullptr;
+                    return tempNode;
+                }
+            }
+            return nullptr;     //In theory this should never happen
+        }
 
 
         //Function: Starts the recursive process of printing the tree.
@@ -72,6 +182,7 @@ class BinarySearchTree {
         //Outputs: None.
         void printTree() {
             preOrder(root);
+            cout << endl;
         }
 
         //Function: Recursively prints the tree in pre order.
--- Assignment6/driver.cpp
+++ Assignment6/driver.cpp
@@ -8,16 +8,47 @@ using std::endl;
 int main() {
     BinarySearchTree tree;
 
+    //Adding to empty tree, then removing the root
     tree.add(50);
-    tree.add(20);
-    tree.add(90);
-    tree.add(40);
-    tree.add(2);
-    tree.add(99);
-    tree.add(1002);
-    tree.add(13);
-    tree.add(67);
+    cout << "Tree pre-order with 50 added: " << endl;
+    tree.printTree();
+    tree.remove(50);
+    cout << "Tree pre-order with 50 removed: " << endl;
+    tree.printTree();
 
+    //Adding 3 balanced nodes to the tree, then removing
+    tree.add(60);
+    tree.add(90);
+    tree.add(30);
+    cout << "Tree pre-order with 60, 90, 30 added: " << endl;
+    tree.printTree();
+    tree.remove(30);
+    cout << "Tree pre-order with 30 removed: " << endl;
+    tree.printTree();
+    tree.remove(60);
+    cout << "Tree pre-order with 60 removed: " << endl;
+    tree.printTree();
+    tree.remove(90);
+    cout << "Tree pre-order with 90 removed: " << endl;
+    tree.printTree();
+
+    //Adding a whole bunch of random numbers and doing random removals
+    tree.add(99);
+    tree.add(34);
+    tree.add(314);
+    tree.add(230);
+    tree.add(16);
+    tree.add(82);
+    tree.add(912);
+    tree.add(65);
+    tree.add(7);
+    cout << "Tree pre-order with various numbers: " << endl;
+    tree.printTree();
+    tree.remove(99);
+    tree.remove(16);
+    tree.remove(314);
+    tree.remove(912);
+    cout << "Tree pre-order with 99, 16, 314, 912 removed: " << endl;
     tree.printTree();
 
     return 0;
