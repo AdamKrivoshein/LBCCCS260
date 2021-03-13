65d103e1f2860d0f2e6624c93643fb03fdd1c4ae
Adam Krivoshein <adamskrivo@gmail.com>
2021-03-13 12:25:20 -0800

    More design and beginnings of code.

--- AssignmentFinal/README.md
+++ AssignmentFinal/README.md
@@ -16,16 +16,16 @@
         expandMatrix()
         size = size + 1
 
-
 Idea: Add addVertex(vertextName, destination) as an overflow function that also calls addEdge().
 
 **addEdge(source, destination):**
+
+    graphMatrix at source, destination = 1
 
 **expandMatrix():**
 
     for each matrix row:
         for each matrix column:
-
 
 **shortestPath(source, destination):**
 
---
+++ AssignmentFinal/graph.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class graph {
+    private:
+        //https://stackoverflow.com/questions/16001803/pointer-to-pointer-dynamic-two-dimensional-array
+        int *matrix = new int[4][4];
+        int matrixSize = 4;
+        int graphSize = 0;
+
+    public:
+        void addVertex() {
+            //if (graphSize < )
+        }
+};
